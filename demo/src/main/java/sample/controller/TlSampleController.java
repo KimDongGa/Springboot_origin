e8e53e9a0c20b37db71a46e5947bc0c72bbfe7a6
dgkim <dgkim@LAPTOP-RCR6NUIE>
2022-07-21 16:29:57 +0900

    22.07.21 Update

---
+++ demo/src/main/java/sample/controller/TlSampleController.java
@@ -0,0 +1,25 @@
+package sample.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@RequestMapping("/")
+@Controller
+public class TlSampleController {
+
+	@GetMapping("/")
+	public String hello(Model model) {
+		model.addAttribute("hello","서버에서 보내준 값");
+		return "/index";
+	}
+
+//	@GetMapping("/")
+	public String test(Model model) {
+//		model.addAttribute("hello","서버에서 보내준 값");
+		return "/leaf";
+	}
+
+}
