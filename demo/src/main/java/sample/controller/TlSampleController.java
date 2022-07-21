623e4be123960bb2f31040cad8e09bcc1d8b2a86
dgkim <dgkim@LAPTOP-RCR6NUIE>
2022-07-21 17:22:41 +0900

    Test View

--- demo/src/main/java/sample/controller/TlSampleController.java
+++ demo/src/main/java/sample/controller/TlSampleController.java
@@ -6,20 +6,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@RequestMapping("/")
 @Controller
 public class TlSampleController {
 
 	@GetMapping("/")
 	public String hello(Model model) {
-		model.addAttribute("hello","서버에서 보내준 값");
-		return "/index";
+		model.addAttribute("myName", "ThymeLeaf");
+		return "index";
 	}
 
 
-//	@GetMapping("/")
 	public String test(Model model) {
-//		model.addAttribute("hello","서버에서 보내준 값");
 		return "/leaf";
 	}
 
